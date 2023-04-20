a5af7d88acdacb49af3fdc5933e6f3e794b437bb
Alexis Ortiz <83848928+ortizal19009@users.noreply.github.com>
2023-04-19 23:59:50 -0500

    Agregar archivos de proyecto.

---
+++ helloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+var chrismasDay = Convert.ToDateTime($"{currentDate.Year}-12-25");
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate.ToString("dd/MM/yyyy")}{Environment.NewLine}Christmas Day: { chrismasDay.ToString("dd/MM/yyyy")}");
+var result = (chrismasDay-currentDate).TotalDays;
+Console.WriteLine($"{Environment.NewLine}The result is: {result}");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
