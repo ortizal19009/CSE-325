475a96e28152179e7fd0a97b479f9f8b4df76a64
Alexis Ortiz Rosero <ortizal@byui.edu>
2023-06-10 20:20:48 -0500

    Updates

--- MegaDeskWeb/MegaDeskWeb/Pages/Quotes/Index.cshtml.cs
+++ MegaDeskWeb/MegaDeskWeb/Pages/Quotes/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MegaDeskWeb.Data;
 using MegaDeskWeb.Modelos;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MegaDeskWeb.Pages.Quotes
 {
@@ -20,13 +21,26 @@ namespace MegaDeskWeb.Pages.Quotes
         }
 
         public IList<Quote> Quote { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            if (_context.Quote != null)
+            var quote = from q in _context.Quote select q;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                quote = quote.Where(s => s.customerName.Contains(SearchString));
+            }
+            Quote = await quote.ToListAsync();
+            /*if (_context.Quote != null)
             {
                 Quote = await _context.Quote.ToListAsync();
-            }
+            }*/
         }
     }
 }
