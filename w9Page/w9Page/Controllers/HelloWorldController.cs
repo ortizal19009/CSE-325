a0bfd193fe5b2466aecb56d78de82d73fc57a35e
Alexis Ortiz Rosero <ortizal@byui.edu>
2023-06-18 00:51:30 -0500

    Create movie App

---
+++ w9Page/w9Page/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace w9Page.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            // return View();
+            return "This is my default action...";
+        }
+        public string Wellcome()
+        {
+            return "this is the Welcome action method";
+        }
+    }
+}
