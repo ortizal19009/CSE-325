a3a5ecbb2eb70f4e3e3c79d6e0250b5e0c1dce8b
Alexis Ortiz Rosero <ortizal@byui.edu>
2023-07-08 18:34:58 -0500

    update sting length

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50)]
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
