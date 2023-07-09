43652dc555a790bfc0af70cd8467dd62cdffece1
Alexis Ortiz Rosero <ortizal@byui.edu>
2023-07-09 00:43:15 -0500

    Create WebBookApi

--- BookStoreApi/BookStoreApi/Program.cs
+++ BookStoreApi/BookStoreApi/Program.cs
@@ -4,28 +4,30 @@ using BookStoreApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddRazorPages();
 builder.Services.Configure<BookStoreDatabaseSettings>(
     builder.Configuration.GetSection("BookStoreDatabase"));
 builder.Services.AddSingleton<BooksService>();
 
+builder.Services.AddControllers()
+     .AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
+app.UseHttpsRedirection();
 
-//app.UseRouting();
+app.UseAuthorization();
 
-//app.UseAuthorization();
-
-//app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
